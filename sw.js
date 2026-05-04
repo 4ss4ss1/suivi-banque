7464f6a74e1d258eab65bc8ea893c91030de1c38
4ss4ss1 <119786204+4ss4ss1@users.noreply.github.com>
2026-05-04 21:55:04 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,3 @@
-// Service Worker minimal qui ne fait rien
-// Juste assez pour permettre l'installation
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', () => {
-  clients.claim();
-});
-
-// On n'intercepte aucune requête
-// => aucun bug de chargement
+// Ce service worker ne fait rien
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => clients.claim());
